fd2bf024543ac17395474b3bbfdc2dbb1ca6ef58
Juno Jupiter <44355558+juno-jupiter@users.noreply.github.com>
2025-06-12 00:31:26 -0400

    correcciones hito3

--- hito3/src/App.jsx
+++ hito3/src/App.jsx
@@ -1,8 +1,8 @@
 import './assets/css/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNavbar from './components/MyNavbar';
-// import Home from './components/Home';
 import Footer from './components/Footer';
+// import Home from './components/Home';
 // import RegisterPage from './components/RegisterPage';
 // import LoginPage from './components/LoginPage';
 import Cart from './components/Cart';
